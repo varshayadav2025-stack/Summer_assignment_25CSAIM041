cc1e8de1e595f85d4d3a9541a74a376649a3e095
varshayadav2025-stack <varsha.yadav2025@glbajajgroup.org>
2026-06-27 01:46:50 +0530

    Add files via upload

---
+++ day10_program2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n=5;
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <=  2 * i - 1); j++)
+        {
+        
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ day10_program_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+   int i, j, n = 5;
+   for(i = 1; i <= n; i++)
+   {
+       for(j = 1; j <= n - i; j++)
+       {
+           printf("%d, j);
+       
+       for(j = 1 - 1; j >= 1; j--)
+       
+           print("%d", j);
+
+       printf("\n");
+   }
+   return 0;    
+}
