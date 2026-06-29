34e878ebc09e42692db0b755386432d5a4a029e0
varshayadav2025-stack <varsha.yadav2025@glbajajgroup.org>
2026-06-29 16:32:36 +0530

    Add files via upload

---
+++ day21_program3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, v = 0, c = 0;
+
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||
+               ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                v++;
+            else
+                c++;
+        }
+    }
+
+    printf("Vowels=%d\nConsonants=%d", v, c);
+
+    return 0;
+}
---
+++ day21_program4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+    }
+
+    puts(str);
+
+    return 0;
+}
